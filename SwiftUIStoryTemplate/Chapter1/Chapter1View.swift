c1377533d7d0760f1da4be1b47ace14be08af136
RachelRussell <rerussell92@gmail.com>
2023-11-15 11:22:15 -0500

    first story

--- SwiftUIStoryTemplate/Chapter1/Chapter1View.swift
+++ SwiftUIStoryTemplate/Chapter1/Chapter1View.swift
@@ -2,7 +2,7 @@ import SwiftUI
 
 struct Chapter1View: View {
     var body: some View {
-        Text("Chapter 1")
+        Text("There once was a 15 year old girl named Thea who lived with her aunt and uncle in a small town called Meadow Ridge .  Thea moved in with her aunt and uncle when she was 6 years old, after her parents Margo and Robin. suddenly died on a crisp  Fall night in the town square. Thea was comfortable living in Meadow Ridge, she had friends and knew all the town elders. People loved Thea and her family trusted her to go into town on her own. The only rule was do not go to the house on the hill, Cobra House.  Cobra House is a dusty old  house that sits on the tallest hill in Meadow Ridge. It overlooks the entire town. It is rumored that cobra house is where the cobra woman lives, a woman who turns into a cobra and haunts anyone who comes to the house.  At night some say you can hear a faint wisper of her chanting Cobra, Cobra, Cobra.  All her life Thea lived in Meadow Ridge but never actually saw the cobra woman.")
     }
 }
 
