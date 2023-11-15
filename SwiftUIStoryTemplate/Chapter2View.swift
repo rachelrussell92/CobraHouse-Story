95ef48ee853d6d904220b741255ba5d06a8ad5c0
Akachi Nwadike <ladysekemi@icloud.com>
2023-11-15 11:30:59 -0500

    chapter 2 1

--- SwiftUIStoryTemplate/Chapter2View.swift
+++ SwiftUIStoryTemplate/Chapter2View.swift
@@ -1,8 +1,22 @@
 import SwiftUI
 
+
 struct Chapter2View: View {
+    
+    
+   
     var body: some View {
-        Text("Chapter 2")
+        
+        VStack {
+            Text("Leah sat at her window  staring at the  Cobra House and wonders why everyone in town is so scarred of it. “I could find out for myself” she thinks to herself.She is home alone and there’s no one or nothing to stop her now she thinks to herself with the widest grin possible. She makes herself a sandwich and stuffs her face reminding herself that every minute counts if she wants to take her time exploring and be back before her aunt and uncle are back home.She dashes into the kitchen and empties out the groceries from her aunties sling bag and dashes back into her room with the bag.")
+            
+            Text("One and a half hour later, she is at the bottom of the hill. She is exhausted but thinks to herself “I’ve come too far to back down now, one little hill wouldn’t hurt.” 30 minutes later, “this does hurt, like a whole lot” she cries to herself. 23 minutes later, she arrives at the Cobra House. There is a strange eerie feeling and an impending sense of doom in the air. “It’s probably just placebo Leah“, she thinks to herself. “Stop whining and let’s get this over with.")
+            
+            
+        
+           
+         
+        }
     }
 }
 
